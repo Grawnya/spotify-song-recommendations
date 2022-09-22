5240d765608b920cfcc9e0dacb0b292bc59d6acc
Grainne Donegan <grainnejdonegan@gmail.com>
2022-09-22 19:16:36 +0000

    create basic wireframe for project

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# import gspread libs and pandas
+
+# set up gspread and google drive api
+
+# read spotify tracks db from kaggle
+
+# ask questions re favourite song, genre, song of all time 
+# (for song ask for artist first and then print all of their songs on console)
+
+# ask mood questions - check if valid y or n
+
+# pick all songs from their favourite artist and all songs that are from that genre and meet the mood criteria
+
+# randomly select up to 20 to be returned
+
+# paste them into google sheet
+
+# open the link in separate tab
+
+# ask to play again
