367cdb9935fc136204fe632feda1ffa2566f3c4f
Grainne Donegan <grainnejdonegan@gmail.com>
2022-09-22 19:30:45 +0000

    connect to song_recs Google sheet

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
-# import gspread libs and pandas
+import gspread
+from google.oauth2.service_account import Credentials
 
-# set up gspread and google drive api
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDENTIALS = Credentials.from_service_account_file('creds.json')
+SCOPE_CREDENTIALS = CREDENTIALS.with_scopes(SCOPE)
+GSPREAD_AUTHORIZATION = gspread.authorize(SCOPE_CREDENTIALS)
+SHEET = GSPREAD_AUTHORIZATION.open('song_recs')
 
 # read spotify tracks db from kaggle
 
