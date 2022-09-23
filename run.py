d42fead00c5522f3bbb9dcfaed98a4bb995d5c3f
Grainne Donegan <grainnejdonegan@gmail.com>
2022-09-23 20:11:56 +0000

    create Artist subclass

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import gspread
 import readline
 from spotify import *
-import pandas as pd
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -14,52 +13,7 @@ CREDENTIALS = Credentials.from_service_account_file('credentials.json')
 SCOPE_CREDENTIALS = CREDENTIALS.with_scopes(SCOPE)
 GSPREAD_AUTHORIZATION = gspread.authorize(SCOPE_CREDENTIALS)
 SHEET = GSPREAD_AUTHORIZATION.open('song_recs')
-
-
-def format_list_values(list_of_values):
-    '''docstring'''
-    lower_list = [each.lower() for each in list_of_values]
-    replace_and = [each.replace('&', 'and') for each in lower_list]
-    replace_apostrophe = [each.replace("'", '') for each in replace_and]
-    replace_full_stop = [each.replace('.', '') for each in replace_apostrophe]
-    replace_e = [each.replace('é', 'e') for each in replace_full_stop]
-    return replace_e
-
-def get_spotify_data():
-    '''
-    get list of music artists and genres from database and
-    also return the database
-    '''
-    spotify_df = pd.read_csv('SpotifyFeatures.csv')
-    music_artists = format_list_values(spotify_df['artist_name'].to_list())
-    genres = format_list_values(spotify_df['genre'].to_list())
-    tracks = format_list_values(spotify_df['track_name'].to_list())
-    return spotify_df, music_artists, genres, tracks
-
-
-def favourite(list_of_interest):
-    '''docstring'''
-    value_of_interest = format_list_values([input('\n')])
-    while value_of_interest[0] not in list_of_interest \
-            and value_of_interest[0] != '':
-        value_of_interest = format_list_values([input('Invalid value. Enter'
-                                                 ' a new value:\n')])
-    return (value_of_interest[0])
-
-
-def favourite_artist(music_artists):
-    '''docstring'''
-    print('Welcome to the Spotify Song Recommender!\n'
-          'We\'ll help you picksome songs that will '
-          'become your new favourites!\n\n'
-          'Firstly we need to ask you some questions!'
-          '\n1. Who is your favourite music artist?\n'
-          'Examples include Cardi B, The 1975 and '
-           'Beyonce:\n')
-    unique_music_artists = list(set(music_artists))
-    music_artist = favourite(unique_music_artists)
-    return music_artist
-    
+   
 
 def favourite_genre(genres):
     '''docstring'''
@@ -79,7 +33,7 @@ def favourite_track(list_of_all_artists, tracks):
           'song and then we\'ll show you all their songs in '
           'our database. Pick your favourite song or you can '
           'pick another artist if you don\'t like the choice\n'
-          'Choose an artist\' discography you want to see:)
+          'Choose an artist\' discography you want to see:')
     unique_music_artists = list(set(list_of_all_artists))
     artist_who_sings = favourite(unique_music_artists)
     picked_track = Spotify(artist_who_sings, tracks)
@@ -102,8 +56,7 @@ def favourite_track(list_of_all_artists, tracks):
 
 # ask to play again
 def main():
-    spotify, music_artists, genres, tracks = get_spotify_data()
-    singer = favourite_artist(music_artists)
-    genre = favourite_genre(genres)
+    singer = Artist().favourite_artist_exists()
+    print(singer)
 
 main()
--- spotify.py
+++ spotify.py
@@ -1,14 +1,13 @@
 import regex
+import pandas as pd
 
 class Spotify:
 
-    def __init__(self, value, type):
-        self.value = value
-        self.type = type
+    def __init__(self):
         self.spotify_db = self._get_spotify_data()
-        self.music_artists = self.format_list_values(spotify_df['artist_name'].to_list())
-        self.genres = self._format_list_values(spotify_df['genre'].to_list())
-        self.tracks = self._format_list_values(spotify_df['track_name'].to_list())
+        self.music_artists = self._format_list_values(self.spotify_db['artist_name'].to_list())
+        self.genres = self._format_list_values(self.spotify_db['genre'].to_list())
+        self.tracks = self._format_list_values(self.spotify_db['track_name'].to_list())
 
     
     def _get_spotify_data(self):
@@ -31,14 +30,26 @@ class Spotify:
     
     def _favourite(self, list_of_interest):
         '''docstring'''
-        value_of_interest = format_list_values([input('\n')])
+        value_of_interest = self._format_list_values([input('\n')])
         while value_of_interest[0] not in list_of_interest \
                 and value_of_interest[0] != '':
-            value_of_interest = format_list_values([input('Invalid value. Enter'
+            value_of_interest = self._format_list_values([input('Invalid value. Enter'
                                                     ' a new value:\n')])
         return (value_of_interest[0])
 
-    def artist_exists(self):
-        '''docstring'''
-        if self.artist_name in self.tracks:
-            print('artist exists')
+class Artist(Spotify):
+
+    def favourite_artist_exists(self):
+        print('Welcome to the Spotify Song Recommender!\n'
+          'We\'ll help you picksome songs that will '
+          'become your new favourites!\n\n'
+          'Firstly we need to ask you some questions!'
+          '\n1. Who is your favourite music artist?\n'
+          'Examples include Cardi B, The 1975 and '
+           'Beyonce:\n')
+        unique_music_artists = list(set(self.music_artists))
+        music_artist = self._favourite(unique_music_artists)
+        return music_artist
+
+
+# class Track(Spotify):
